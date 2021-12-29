e208072fb18ea0bd606ecfe04cc2e98b6b2f20a2
muhammed-bayat <muhammed.bayat07@erzurum.edu.tr>
2021-12-29 03:49:52 +0300

    test

--- App.js
+++ App.js
@@ -1,20 +1,56 @@
-import { StatusBar } from 'expo-status-bar';
-import { StyleSheet, Text, View } from 'react-native';
+import React from 'react';
+import {
+  Text,
+  SafeAreaView,
+  FlatList,
+  StyleSheet,
+  TextInput,
+} from 'react-native';
+import ProductCard from './src/components/ProductCard';
+import products_data from './data.json';
 
-export default function App() {
+const App = () => {
+  const renderProduct = ({item}) => <ProductCard product={item} />;
   return (
-    <View style={styles.container}>
-      <Text>Open up App.js to start working on your app!</Text>
-      <StatusBar style="auto" />
-    </View>
+    <SafeAreaView>
+      <Text style={styles.head}>PATİKASTORE</Text>
+      <TextInput
+        inlineImageLeft="search_icon"
+        style={styles.input}
+        placeholder="Ara..."
+      />
+      <FlatList
+        columnWrapperStyle={styles.tagView}
+        numColumns={2}
+        data={products_data}
+        renderItem={renderProduct}
+      />
+    </SafeAreaView>
   );
-}
-
+};
 const styles = StyleSheet.create({
-  container: {
-    flex: 1,
-    backgroundColor: '#fff',
-    alignItems: 'center',
-    justifyContent: 'center',
+  tagView: {
+    flexDirection: 'row',
+    justifyContent: 'space-between',
+  },
+  input: {
+    height: 50,
+    margin: 5,
+    borderRadius: 20,
+    backgroundColor: '#eceff1',
+  },
+  head: {
+    fontWeight: 'bold',
+    fontSize: 30,
+     
+    marginTop: 60,
+    marginLeft: 10,
+    marginBottom: 10,
+  
+ 
+   
+    color: 'purple',
   },
 });
+
+export default App;
